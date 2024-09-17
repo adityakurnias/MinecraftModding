7c0aa23f2699eb3a50069a340981d580f7faece7
adityakurnias <adit34072@gmail.com>
2024-09-17 17:11:39 +0700

    Init Project

---
+++ src/main/java/com/kurnias/nyobamodding/Client.java
@@ -0,0 +1,11 @@
+package com.kurnias.nyobamodding;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
